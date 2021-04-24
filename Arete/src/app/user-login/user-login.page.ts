510c12dd97c91e66f8004529082d7e96ef974d5d
djinnsend <nanaboakye1738@gmail.com>
2021-04-24 19:39:44 +0000

    Successfully tested post with fetch()

--- Arete/src/app/user-login/user-login.page.ts
+++ Arete/src/app/user-login/user-login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-login',
@@ -7,13 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginPage implements OnInit {
 
-  username: string = '';
-  email: string = '';
-  password: string = '';
+  username: string = 'ADMIN';
+  email: string = 'admin@admin.com';
+  password: string = '1234';
   return: Array<object>;
-  constructor() { }
+  data:object;
+  constructor(private HTTP:HTTP, private client:HttpClient) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.HTTP.post('http://localhost/final-Djinnsend/php/api/User/getUserLogin.php', {"Username": `${this.username}`,"Email": `${this.email}`, "Password": `${this.password}`}, {});
+    // console.log(this.return);
+    console.log("hello");
+  }
+
+  fetch(){
+    // this.client.get('http://localhost/final-Djinnsend/php/api/User/getAllEvents.php').subscribe (data => {
+    //   console.log(data);
+    // })
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    // const requestOptions = new RequestOptions({ headers: headers });
+    this.data = {"Username": this.username,"Email": this.email, "Password": this.password};
+    this.client.post('http://localhost/final-Djinnsend/php/api/User/getUserLogin.php', JSON.stringify(this.data), {}).subscribe(data => {
+      console.log(data['_body']);
+    });
+    console.log("hello");
   }
 
 }
